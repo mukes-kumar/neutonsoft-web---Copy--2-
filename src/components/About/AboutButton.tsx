ad7a940bb30d27aee9467830357938da44d63b8d
Mukesh Prajapati <145520335+mukes-kumar@users.noreply.github.com>
2024-10-10 00:26:27 +0530

    changes about page

--- src/components/About/AboutButton.tsx
+++ src/components/About/AboutButton.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect } from 'react'
 import Image from 'next/image';
-// import ScrollReveal from 'scrollreveal';
+import ScrollReveal from 'scrollreveal';
 
 
 interface StyleProps {
@@ -13,24 +13,24 @@ function AboutButton() {
     color:'transparent',
   };
 
-  //   const sr = ScrollReveal({
-  //   distance: '60px',
-  //   duration: 2500,
-  //   delay: 400,
-  //   // reset: true
-  // });
+    const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+  });
 
-  // useEffect(() => {
-  //   sr.reveal('.home__header, .section__title', { delay: 600 });
-  //   sr.reveal('.home__footer', { delay: 700 });
-  //   sr.reveal('.home__img', { delay: 900, origin: 'top' });
+  useEffect(() => {
+    sr.reveal('.home__header, .section__title', { delay: 600 });
+    sr.reveal('.home__footer', { delay: 700 });
+    sr.reveal('.home__img', { delay: 900, origin: 'top' });
 
-  //   sr.reveal('.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy', { origin: 'top', interval: 100 });
-  //   sr.reveal('.specs__data, .discount__animate', { origin: 'left', interval: 100 });
-  //   sr.reveal('.specs__img, .discount__img', { origin: 'right' });
-  //   sr.reveal('.case__img', { origin: 'top' });
-  //   sr.reveal('.case__data');
-  // }, );
+    sr.reveal('.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy', { origin: 'top', interval: 100 });
+    sr.reveal('.specs__data, .discount__animate', { origin: 'left', interval: 100 });
+    sr.reveal('.specs__img, .discount__img', { origin: 'right' });
+    sr.reveal('.case__img', { origin: 'top' });
+    sr.reveal('.case__data');
+  }, );
   return (
     <>
        <div className="home__header relative overflow-hidden bg-gray-50 pt-16 pb-32 space-y-24">
